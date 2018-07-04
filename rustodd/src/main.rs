51c400bb5a1dfffc44dd73022f815b4c8053e28e
L. P <2953293+Nemin32@users.noreply.github.com>
2018-07-04 22:01:13 +0200

    Documented the client.

--- rustodd/src/main.rs
+++ rustodd/src/main.rs
@@ -23,22 +23,19 @@ extern crate serde_derive;
 extern crate bincode;
 use bincode::{serialize, deserialize};
 
-//375 x 260
+// Every screen is 375 x 260.
 const ROOM_WIDTH: u16 = 375;
 const ROOM_HEIGHT: u16 = 260;
-
-//640 x 480
-//const PROPORTION_W: f64 = 800.0 / ROOM_WIDTH as f64;
-//const PROPORTION_H: f64 = 640.0 / ROOM_HEIGHT as f64;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct PlayerInfo {
     name: String,
     saved_muds: u16,
-    location: [u16; 3],
-    position: [u16; 2]
+    location: [u16; 3], //LVL ID, Path ID, CAM ID
+    position: [u16; 2] // X, Y
 }
 
+// I don't really know how this works. I copied this from here: https://github.com/retep998/winapi-rs
 fn into_os(msg: &str) -> Vec<u16> {
     use std::ffi::OsStr;
     use std::iter::once;
@@ -47,6 +44,7 @@ fn into_os(msg: &str) -> Vec<u16> {
     OsStr::new(msg).encode_wide().chain(once(0)).collect()
 }
 
+// Prompts the user and returns their answer, stripped of newline
 fn read_line(prompt: &str) -> String {
     let mut retval: String = String::new();
     println!("{} ", prompt);
@@ -58,6 +56,8 @@ fn read_line(prompt: &str) -> String {
     retval
 }
 
+// Connects to the server. Currently the port is fixed, but this would be easy to fix.
+// TODO: Make the client/server port agnostic.
 fn make_connection() -> TcpStream {
     let ip = read_line("Enter the server's IP: ");
     
@@ -71,52 +71,72 @@ fn make_connection() -> TcpStream {
     connection
 }
 
+// Now this is where things get a bit more interesting. This function renders red rectangles at the location of every player.
 fn render_players(relativexy: Receiver<[u16; 2]>) {
     use winapi::shared::windef::RECT;
     use winapi::um::winuser::GetWindowRect;
 
     unsafe {
-    let oddapp = FindWindowW(null_mut(), into_os("Oddworld Abe's Exoddus").as_ptr());
+    let oddapp = FindWindowW(null_mut(), into_os("Oddworld Abe's Exoddus").as_ptr()); // HWND to the game's window.
     let hdc = winapi::um::winuser::GetDC(oddapp);
     
     let mut winrect = RECT {left: 0, top: 0, right: 0, bottom: 0};
-    GetWindowRect(oddapp, &mut winrect);
+    GetWindowRect(oddapp, &mut winrect); // We query the window to get it's dimensions. Should be about 600x800, but it varies.
     
     let PROPORTION_W: f64 = (winrect.right - winrect.left) as f64 / ROOM_WIDTH as f64;
     let PROPORTION_H: f64 = (winrect.bottom - winrect.top) as f64 / ROOM_HEIGHT as f64;
     
-    let brush = winapi::um::wingdi::CreateSolidBrush(winapi::um::wingdi::RGB(255, 0, 0));
+    let brush = winapi::um::wingdi::CreateSolidBrush(winapi::um::wingdi::RGB(255, 0, 0)); // We create the aforementioned red color for our rectangle.
     winapi::um::wingdi::SelectObject(hdc, brush as LPVOID);
     
     loop {
-        let relativexy = relativexy.recv().unwrap();
-        winapi::um::wingdi::Rectangle(
-            hdc,
-            ((relativexy[0] as i16 - 4) as f64 * PROPORTION_W) as i32,
-            ((relativexy[1] as i16 - 32) as f64 * PROPORTION_H) as i32,
-            ((relativexy[0] + 14) as f64 * PROPORTION_W) as i32,
-            ((relativexy[1] - 16) as f64 * PROPORTION_H) as i32);
-        };
+        let relativexy = relativexy.recv().unwrap(); // When the main thread sends us coordinates, we receive them into this array.
+        
+        // And we draw a rectangle. The sizes are mostly done in a "what looks good fashion".
+        
+        if relativexy[0] != 0 && relativexy[1] != 0 {
+            winapi::um::wingdi::Rectangle(
+                hdc,
+                ((relativexy[0] as i16 - 8) as f64 * PROPORTION_W) as i32,
+                ((relativexy[1] as i16 - 32) as f64 * PROPORTION_H) as i32,
+                ((relativexy[0] + 10) as f64 * PROPORTION_W) as i32,
+                ((relativexy[1] - 16) as f64 * PROPORTION_H) as i32);
+            };
+            
+            let name = "TESZT";
+            let name = into_os(name);
+            winapi::um::wingdi::TextOutW(
+                hdc, 
+                ((relativexy[0] as i16 - 10) as f64 * PROPORTION_W) as i32, 
+                ((relativexy[1] as i16 - 20) as f64 * PROPORTION_W) as i32, 
+                name.as_ptr(), name.len() as i32 - 1);
+        }
     }
 }
 
 fn main() {
     unsafe {
-        let oddapp = FindWindowW(null_mut(), into_os("Oddworld Abe's Exoddus").as_ptr());
+        let oddapp = FindWindowW(null_mut(), into_os("Oddworld Abe's Exoddus").as_ptr()); // HWND to the window.
         let mut proc: DWORD = 0;
-        let access: DWORD = PROCESS_VM_READ | PROCESS_QUERY_INFORMATION;
+        
+        // Since we aren't actually writing anything into the game's memory, these are all the privieges we need.
+        let access: DWORD = PROCESS_VM_READ | PROCESS_QUERY_INFORMATION; 
 
-        if oddapp == null_mut() /*false*/ { //Remove comment to turn on checking.
+        if oddapp == null_mut() /*false*/ { // Set this to just 'false' to avoid the check.
             println!("You need to run Exoddus.exe before starting this program.");
             process::exit(1);
         } 
         
-        GetWindowThreadProcessId(oddapp, &mut proc);
+        GetWindowThreadProcessId(oddapp, &mut proc); 
         
-        let handle = OpenProcess(access, 0, proc);
-        let mut read: u16 = 0;
-        let mut old: u16 = 1;
-        let readp: *mut u16 = &mut read;
+        let handle = OpenProcess(access, 0, proc); // We obtain the HANDLE to the underlying process.
+        
+        // You might be wondering about this pattern where I make explicit pointers to the variables.
+        // It's not my sillyness, Rust demands you to be this precise. This results in a few unnecessary lines of code.
+        
+        let mut saved_muds: u16 = 0;
+        let mut previously_muds: u16 = 0;
+        let saved_mudsp: *mut u16 = &mut saved_muds;
         
         let mut pos: [u16; 3] = [0; 3];
         let mut prevpos: [u16; 3] = [0; 3];
@@ -128,83 +148,108 @@ fn main() {
 
         let (sender, receiver) = channel();
         
-        thread::spawn(move || {render_players( receiver)});
+        thread::spawn(move || {render_players(receiver)});
+        
+        
+        ////// This is some confusing mess. //////
+        
+        /*
+        Writing this was a real pain. Mostly I pointer juggled, until finally I understood how it should be done.
+        If you want to understand how this works, I suggest that you read through 'second.cpp' 
+        and insert the values below this into something like Cheat Engine.
+        For CE to work, you'll need a Pointer, with two offsets and you have to read 2 bytes. The first (lower) offset is always 0x68.
+        But as I said, this was mostly shooting in the dark, so if by any chance, you're struggling, it's ok mate. I did too.
+        
+        Abe_x --- Exoddus.exe+0x1C1230, 0x68, 0xBA
+        Abe_y --- Exoddus.exe+0x1C1230, 0x68, 0xBE
+        */
         
         use winapi::um::psapi::MODULEINFO;
         use winapi::um::psapi::GetModuleInformation;
         
+        // Actually this is a hack, since EnumProcessModules returns an array, in which we should have to manually search for the appropriate module.
+        // But in AE's case, the first module is Exoddus.exe itself, so we can load it like this no problem.
         let mut module: HMODULE = null_mut();
-        winapi::um::psapi::EnumProcessModules(handle, &mut module, 8, null_mut());
+        winapi::um::psapi::EnumProcessModules(handle, &mut module, 8, null_mut()); 
         
+        // This whole thing is only necessary to get the base address in a format we can actually do calculations with.
         let mut mi: MODULEINFO = MODULEINFO {lpBaseOfDll: null_mut(), SizeOfImage: 0, EntryPoint: null_mut()};
         GetModuleInformation(handle, module, &mut mi, 24);
+        let base_pointer = (mi.lpBaseOfDll as u32 + 0x1C1230) as *mut u8; // Exoddus.exe+0x1C1230
 
-        let base_pointer = (mi.lpBaseOfDll as u32 + 0x1C1230) as *mut u8;
-
+        // This pointer is actually holding ANOTHER pointer, which we need to offset by either 0xBA or 0xBE to get the X or Y coordinates.
         let pointer: u32 = 0;
         let pp: *const u32 = &pointer;
-        
         ReadProcessMemory(handle, base_pointer as LPVOID, pp as LPVOID, size_of::<u32>(), null_mut());
-
-        let pos_base = (pointer + 0x68) as *mut u32;
+        let pos_base = (pointer + 0x68) as *mut u32; 
 
         let xpos: u16 = 0;
         let xp: *const u16 = &xpos;
-        ReadProcessMemory(handle, pos_base as LPCVOID, pp as LPVOID, size_of::<u32>(), null_mut());
-        ReadProcessMemory(handle, (pointer + 0xBA) as LPCVOID, xp as LPVOID, size_of::<u16>(), null_mut());
         
         let ypos: u16 = 0;
         let yp: *const u16 = &ypos;
-        ReadProcessMemory(handle, pos_base as LPCVOID, pp as LPVOID, size_of::<u32>(), null_mut());
-        ReadProcessMemory(handle, (pointer + 0xBE) as LPCVOID, yp as LPVOID, size_of::<u16>(), null_mut());
+        ////// Confusing mess ends here. //////
         
+        // This HashMap contains the players' data. See PlayerInfo struct.
         let mut players = HashMap::new();
         
         loop {
-            //0x5C1BC2 DWORD number of rescued Mudokons
-            //0x5C3030 - 0x5C3034 3xDWORD = LVL ID, PATH ID, CAM ID
+            // 0x5C1BC2 DWORD number of rescued Mudokons
+            // 0x5C3030 - 0x5C3034 3xDWORD = LVL ID, PATH ID, CAM ID
 
-            ReadProcessMemory(handle, 0x5C1BC2 as LPCVOID, readp as LPVOID, size_of::<u16>(), null_mut()); 
+            // Reading the number of saved Mudokons.
+            ReadProcessMemory(handle, 0x5C1BC2 as LPCVOID, saved_mudsp as LPVOID, size_of::<u16>(), null_mut()); 
+            
+            // Reading the current LVL/Path/CAM ID-s.
             ReadProcessMemory(handle, 0x5C3030 as LPCVOID, pos.as_mut_ptr() as LPVOID, size_of::<u16>() * 3, null_mut());
             
+            // We offset the pointer by 0xBA and thus we can read the X coordinate.
             ReadProcessMemory(handle, pos_base as LPCVOID, pp as LPVOID, size_of::<u32>(), null_mut());
             ReadProcessMemory(handle, (pointer + 0xBA) as LPCVOID, xp as LPVOID, size_of::<u16>(), null_mut());
             
+            // We offset by 0xBE and like before we read the Y coordinate.
             ReadProcessMemory(handle, pos_base as LPCVOID, pp as LPVOID, size_of::<u32>(), null_mut());
             ReadProcessMemory(handle, (pointer + 0xBE) as LPCVOID, yp as LPVOID, size_of::<u16>(), null_mut());
 
+            // The player coordinates are absolute values. The top left of the *entire map* is [0, 0], not the current room.
+            // To turn the coordinates into the format we need 
+            // We modulo the xpos and the ypos we get a value between [0, 0] and [ROOM_WIDTH, ROOM_HEIGHT].
             let relativexy: [u16; 2] = [xpos % ROOM_WIDTH, ypos % ROOM_HEIGHT];      
 
-            if old != read || prevpos != pos || prevhero != relativexy {
+            // If anything changed (Abe moved, Mudokons were saved, Abe left the screen), we update the variables and we send the data to the server.
+            if previously_muds != saved_muds || prevpos != pos || prevhero != relativexy {
                 prevpos = pos;
-                old = read;
+                previously_muds = saved_muds;
                 prevhero = relativexy;
                 
-                
-                
-                if pos[0] != 0 || relativexy != [0,0] { //Title screen check
-                    let payload = PlayerInfo {name: name.clone(), saved_muds: read, location: pos, position: relativexy};
-                    let bytes: Vec<u8> = serialize(&payload).unwrap();
+                // Title screen check: If we're on the title screen, we don't want to send data.
+                if pos[0] != 0 || relativexy != [0,0] { 
+                    // This will be sent to the server.
+                    let payload = PlayerInfo {name: name.clone(), saved_muds: saved_muds, location: pos, position: relativexy};
+                    let bytes: Vec<u8> = serialize(&payload).unwrap(); // We use the Serde Bincode crate for this.
                     connection.write_all(bytes.as_slice()).unwrap();
                 }
             }
 
-            
+            // This buffer contains the raw data the server sends us.
             let mut buffer = vec![0;512];
             match connection.read(&mut buffer) {
                 Ok(_) => {
+                    // We receive all the players' data from the server.
                     let msg: HashMap<String, PlayerInfo> = deserialize(&buffer[..]).unwrap();
                     
-                    for (name, plinfo) in msg {
+                    for (name, plinfo) in msg { // We update the players to their newest state.
                         players.insert(name.clone(), plinfo.clone());
                     }
                 },
                 _ => {}
             };
             
-            for (_, vals) in &players {sender.send(vals.position).unwrap();}
+            // If our character is on the same screen as some other player, we send their location for the renderer thread.
+            for (_, vals) in &players { if vals.location == pos {sender.send(vals.position).unwrap();}}
             
-            thread::sleep(time::Duration::from_millis(500));
+            // Finally, we sleep to be less straining on the PC.
+            thread::sleep(time::Duration::from_millis(200));
         }
         
     }
